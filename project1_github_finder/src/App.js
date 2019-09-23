b1a0167ff99b4df195969229835ed099a6241b8d
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2019-09-23 14:36:17 -0400

    notes up to lect9

--- project1_github_finder/src/App.js
+++ project1_github_finder/src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Fragment, component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello from React</h1>
-    </div>
-  );
+class App extends React{
+  render() {
+    return (
+      <Fragment>
+        <h1>Hello from React</h1>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
